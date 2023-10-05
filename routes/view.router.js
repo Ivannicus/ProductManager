32d6c02b569fc0673e8416ec2d8419ed48b6677f
Ivan Gutierrez <ingutierrez@uc.cl>
2023-10-05 18:33:41 -0300

    Funciones implementadas, sort y query funcionando

--- routes/view.router.js
+++ routes/view.router.js
@@ -7,24 +7,50 @@ const router = express.Router();
 
 router.get('/', async (req, res)=> {
     try{
-        const { limit = 10, page = 1, sort, query} = req.query;
+        const { limit = 10, page = 1, sort, query = false} = req.query;
 
 
+        
 
-        const { docs, totalDocs, hasPrevPage, hasNextPage, nextPage, prevPage} = await productsModel.paginate({}, {limit, page, lean:true, sort});
-  
-        const products = docs;
+        
+        let sortType = 0;
+        if (sort === "asc"){
+            sortType = 1
+        };
+        if (sort === "desc"){
+            sortType = -1
+        }
+        
+        if (query) {
+            const objQuery = JSON.parse('{'+query+'}');
+            const { docs, totalDocs, hasPrevPage, hasNextPage, nextPage, prevPage} = await productsModel.paginate(objQuery, {limit, page, lean:true, sort: {price: sortType}});
+            const products = docs;
+            res.render('home', {
+                products,
+                totalDocs,
+                hasNextPage,
+                hasPrevPage,
+                nextPage,
+                prevPage,
+                limit,
+                sort,
+                query
+            });
+        } else {
+            const { docs, totalDocs, hasPrevPage, hasNextPage, nextPage, prevPage} = await productsModel.paginate({}, {limit, page, lean:true, sort: {price: sortType}});
+            const products = docs;
+            res.render('home', {
+                products,
+                totalDocs,
+                hasNextPage,
+                hasPrevPage,
+                nextPage,
+                prevPage,
+                limit,
+                sort
+            });
+        }
 
-        res.render('home', {
-            products,
-            totalDocs,
-            hasNextPage,
-            hasPrevPage,
-            nextPage,
-            prevPage,
-            limit,
-            sort
-        });
     } catch (error){
         res.status(500).send({'error': error})
     }
@@ -35,7 +61,9 @@ router.get('/realtimeproducts', async (req, res)=> {
     
     res.render('realTimeProducts', {});
     const products = await productsModel.find().lean();
+    // res.render('realTimeProducts',{});
     io.emit('products', products); 
+    
 });
 
 router.get('/chat', (req, res) => {
@@ -49,12 +77,51 @@ router.get('/carts/:cid', async (req, res) => {
     const cart = await cartsModel.findOne({_id: idCart}).populate('products.product').lean();
     const products = cart.products;
 
-    console.log(products);
 
     res.render('carts', {
         idCart,
         products
     })
-})
+});
+
+router.get('/products', async (req, res)=> {
+    try{
+        const { limit = 10, page = 1, sort, query} = req.query;
+
+        const objQuery = JSON.parse('{'+query+'}')
+      
+        const { docs, totalDocs, hasPrevPage, hasNextPage, nextPage, prevPage} = await productsModel.paginate(objQuery, {limit, page, lean:true});
+  
+        const products = docs;
+
+        res.render('products', {
+            products,
+            totalDocs,
+            hasNextPage,
+            hasPrevPage,
+            nextPage,
+            prevPage,
+            limit,
+            sort,
+            query
+        });
+    } catch (error){
+        res.status(500).send({'error': error})
+    }
+});
+
+router.get('/products/:pid', async (req, res)=> {
+    try{
+        const idProduct = req.params.pid;
+        const product = await productsModel.findOne({_id: idProduct}).lean();
+        res.render('product', {
+            idProduct,
+            product
+        });
+    } catch (error){
+        res.status(500).send({'error': error})
+    }
+
+});
 
 export default router;
